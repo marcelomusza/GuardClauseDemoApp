1a03eac943387b78eea764a8f54e39bb25d06e4c
Marcelo Muszalski <marcelo.muszalski@dcsl.com>
2023-09-05 18:51:29 +0200

    Demo Console app showcasing the Throw Guards

---
+++ GuardClauseDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using HelperLibrary;
+
+HelperMethods helpers = new();
+
+int decades = helpers.AgeInDecades(43);
+Console.WriteLine($"43 is {decades} decades old");
+
+decades = helpers.AgeInDecades(49);
+Console.WriteLine($"49 is {decades} decades old");
+
+try
+{
+	decades = helpers.AgeInDecades(-200);
+	Console.WriteLine($"-200 is {decades} decades old");
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
+try
+{
+	decades = helpers.AgeInDecades(200);
+	Console.WriteLine($"200 is {decades} decades old");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine();
+
+Console.WriteLine(helpers.TestName("Tim Smith"));
+Console.WriteLine(helpers.TestName("Bob Corey"));
+Console.WriteLine(helpers.TestName("Sue Storm"));
+
+try
+{
+	helpers.TestName("Tim Corey");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    helpers.TestName("");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    helpers.TestName("   ");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    helpers.TestName(null);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ HelperLibrary/HelperMethods.cs
@@ -0,0 +1,49 @@
+﻿namespace HelperLibrary;
+using Throw;
+
+public class HelperMethods
+{
+    public int AgeInDecades(int age)
+    {
+        //if (age < 0)
+        //{
+        //    throw new ArgumentException("The age should not be less than zero.", nameof(age));
+        //}
+
+        //if (age > 130)
+        //{
+        //    throw new ArgumentException("The age should not be greater than 130.", nameof(age));
+        //}
+
+        //age.Throw().IfNegative();
+        //age.Throw().IfGreaterThan(130);
+
+        age.Throw()
+           .IfNegative()
+           .IfGreaterThan(130);
+
+        return age / 10;
+    }
+
+    public string TestName(string? name)
+    {
+        name.ThrowIfNull()
+            .IfEmpty()
+            .IfWhiteSpace()
+            .Throw("Sorry, you cannot use my name.")
+            .IfEqualsIgnoreCase("Tim Corey");
+
+        if (name.Contains("Tim"))
+        {
+            return "Great first name";
+        }
+
+        if (name.Contains("Corey"))
+        {
+            return "Great last name";
+        }
+
+        return "Way to be unique";
+    }
+}
+
